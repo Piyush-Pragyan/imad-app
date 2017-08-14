fe7fd7831c3c8e6c5d444c2588690850dd4e8336
Piyush-Pragyan <30867581+Piyush-Pragyan@users.noreply.github.com>
2017-08-14 11:43:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.get('/article-1',function (req, res){
 });
 app.get('/article-2',function (req, res){
     res.send("Article two is requested an will be served here.")
-})
+});
 app.get('/article-3',function (req, res){
     res.send("Article three is requested an will be served here.")
-})
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
