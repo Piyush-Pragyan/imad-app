f4d85a3675b4487c10df8035d95aaf6978b00793
Piyush-Pragyan <30867581+Piyush-Pragyan@users.noreply.github.com>
2017-08-27 20:03:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,6 +70,10 @@ app.get('/article-1',function (req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-1.html'));
     
 });
+app.get('/article-4',function (req, res){
+    res.sendFile(path.join(__dirname, 'ui', 'article-4.html'));
+    
+});
 app.get('/article-2',function (req, res){
      res.send(createTemplate(articleTwo));
 
